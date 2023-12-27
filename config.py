b9cecd5cebefcef0bf661eea8846dc86577fea0a
Mythili-2002 <67330758+Mythili-2002@users.noreply.github.com>
2023-12-27 11:37:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "myappdb.cjo60u2ccet7.us-east-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "admin123"
-customdb = "employee"
+customdb = "myappdb"
 custombucket = "myappbucket0101"
 customregion = "us-east-1"
